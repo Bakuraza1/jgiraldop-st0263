aad84d5620d7bc6ff052de9660247d91e9956069
Julian Giraldo Perez <jgpbak@outlook.com>
2023-03-06 00:26:53 -0500

    Reto2 codigo despliegue

---
+++ run.sh
@@ -0,0 +1,9 @@
+  GNU nano 6.2                                                                                           run.sh                                                                                                    
+#!/bin/bash
+python3 ./rpc/worker.py & 
+
+python3 ./mom/worker1/worker1.py & 
+
+python3 ./mom/worker2/worker2.py &
+
+python3 ./gateWay/apiGateWay.py  
